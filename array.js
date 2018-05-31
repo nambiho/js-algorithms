02595e408cc6f91f01c8590345b6f675985f2686
nambiho.moon <nambiho@naver.com>
2018-05-31 14:46:35 +0900

    algorithem array

--- array.js
+++ array.js
@@ -1,3 +1,24 @@
+/**
+ * javascript Array Object is best
+ */
 function array () {
 	return []
 }
+
+/**
+ * polyfill : ECMAScript2015
+ */
+(function ($a) {
+	/**
+	 * Method
+	 * 	from
+	 * 	of
+	 * 
+	 * Prototype
+	 * 	copyWithin
+	 * 	fill
+	 * 	find
+	 * 	findIndex
+	 */
+	return $a
+} (array()))
