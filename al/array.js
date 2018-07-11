43939158b13c927abb49580d748e116bdfbec016
nambiho.moon <nambiho@naver.com>
2018-07-11 14:28:38 +0900

    algorithm

---
+++ al/array.js
@@ -0,0 +1,32 @@
+/**
+ * javascript Array Object is best
+ */
+function array () {
+	return []
+}
+
+/**
+ * polyfill : ECMAScript2015
+ */
+(function (A) {
+	/**
+	 * Method
+	 * 	from
+	 * 	of
+	 * 
+	 * Prototype
+	 * 	copyWithin
+	 * 	fill
+	 * 	find
+	 * 	findIndex
+	 */
+
+	A.prototype.from = function () {}
+	A.prototype.of = function () {}
+
+	A.prototype.copyWithin = function () {}
+	A.prototype.fill = function () {}
+	A.prototype.find = function () {}
+	A.prototype.findIndex = function () {}
+
+} (array()))
